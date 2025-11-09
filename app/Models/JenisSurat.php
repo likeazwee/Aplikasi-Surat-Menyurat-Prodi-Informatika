e4480980b8699f4244af8ada162338a95ed4f612
Agyl Wendi Pratama <agylwendipratama09@gmail.com>
2025-11-09 20:14:35 +0700

    Fitur Auto Buat Surat DONE

--- app/Models/JenisSurat.php
+++ app/Models/JenisSurat.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class JenisSurat extends Model
 {
@@ -14,14 +15,23 @@ class JenisSurat extends Model
      *
      * @var string
      */
-    protected $table = 'jenis_surat'; // <-- Beritahu nama tabelnya (tanpa 's')
+    protected $table = 'jenis_surat'; // <-- Pastikan ini nama tabel Anda
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [ // <-- Kolom yang boleh diisi
+    protected $fillable = [
         'nama_surat',
+        'template_file', // <-- INI YANG DITAMBAHKAN
     ];
+
+    /**
+     * Get the pengajuanSurats for the jenis surat.
+     */
+    public function pengajuanSurats(): HasMany
+    {
+        return $this->hasMany(PengajuanSurat::class, 'jenis_surat_id');
+    }
 }
