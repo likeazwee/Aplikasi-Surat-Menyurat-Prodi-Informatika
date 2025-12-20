6a674a74ccc7af7169e3fb5f1910de412eb04ca5
Agyl Wendi Pratama <145989523+likeazwee@users.noreply.github.com>
2025-12-20 11:55:31 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Membuat Akun Admin
+        User::create([
+            'name' => 'Admin Prodi Informatika',
+            'email' => 'adminprodi@gmail.com',
+            'password' => Hash::make('adminprodi123'),
+            'role' => 'admin', // Pastikan kolom 'role' sesuai dengan database Anda
+        ]);
+
+        // Membuat Akun Kaprodi
+        User::create([
+            'name' => 'Kaprodi Informatika',
+            'email' => 'kaprodi@gmail.com',
+            'password' => Hash::make('kaprodi123'),
+            'role' => 'kaprodi',
+        ]);
+    }
+}
